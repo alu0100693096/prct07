84d25a50caa5df5d9b381aec8a72b5c24485741c
Laura Fariña <alu0100693096@ull.edu.es>
2013-10-22 16:41:14 +0100

    Probando si hay denominador

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -3,13 +3,17 @@ require "lib/frac_main.rb"
 describe Fraccion do
 
 	before :each do
-		@f1 = Fraccion.new(1)
+		@f1 = Fraccion.new(1,1)
 	end
 
-	#Debe existir un numerador
 	describe "#Comprobacion de variables" do
+		#Debe existir un numerador
 		it "Hay numerador" do
 			@f1.n.should eq(1)
+		end
+		#Debe existir un denominador
+		it "Hay denominador" do
+			@f1.d.should eq(1)
 		end
 	end
 end
