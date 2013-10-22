f06b00aab01b62c8fdf64d94836b9f51885ca172
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-22 22:23:25 +0100

    Añadido el test que muestra si una fracción está en su forma reducida.
    Contenido del error:
    Failures:
    
      1) Fraccion Debe estar en su forma reducida
         Failure/Error: Unable to find matching line from backtrace
    
           expected: 1
                got: 2
    
           (compared using ==)
         # ./spec/frac_spec.rb:22:in `block (2 levels) in <top (required)>'
    
    Finished in 0.0009 seconds
    3 examples, 1 failure
    
    Failed examples:
    
    rspec /usr/lib/ruby/vendor_ruby/rspec/core/metadata.rb:164 # Fraccion Debe estar en su forma reducida

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -2,18 +2,24 @@ require "lib/fraccion.rb"
 
 describe Fraccion do
 
-	before :each do
-		@f1 = Fraccion.new(1,1)
-	end
+    before :each do
+        @f1 = Fraccion.new(1,1)
+    end
 
-	describe "#Comprobacion de variables" do
-		#Debe existir un numerador
-		it "Hay numerador" do
-			@f1.n.should eq(1)
-		end
-		#Debe existir un denominador
-		it "Hay denominador" do
-			@f1.d.should eq(1)
-		end
-	end
+    describe "#Comprobacion de variables" do
+        #Debe existir un numerador
+        it "Hay numerador" do
+            @f1.n.should eq(1)
+        end
+        #Debe existir un denominador
+        it "Hay denominador" do
+            @f1.d.should eq(1)
+        end
+    end
+
+    it "Debe estar en su forma reducida" do
+        f2 = Fraccion.new(2, 4)
+        f2.n.should eq(1)
+        f2.d.should eq(2)
+    end
 end
