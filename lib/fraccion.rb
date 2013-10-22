8a58ffb89fdf2a280ab7b7e663c866ae3549960b
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-22 22:40:19 +0100

    Añadido el código que permite que pase el test

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -1,6 +1,37 @@
 class Fraccion
-	attr_reader :n, :d
-	def initialize(n,d)
-		@n,@d = n,d
-	end
+    attr_reader :n, :d
+    def initialize(n,d)
+        @n,@d = n,d
+        reducir
+    end
+
+private
+
+    # Pasa la fracción actual a su forma más reducida y coloca el signo de la
+    # fracción en el numerador.
+    def reducir
+        # Si alguno es negativo, que sea el numerador. Si ambos son negativos,
+        # se pasan a positivos.
+        if @d < 0
+            @n = -@n
+            @d = -@d
+        end
+
+        # Para reducir la fracción se dividen numerador y denominador entre el
+        # máximo común divisor de ambos.
+        mcd = gcd(@n, @d)
+        if mcd != 0
+            @n /= mcd
+            @d /= mcd
+        end
+    end
+
+    # Calcula el máximo común divisor entre dos números enteros a y b.
+    def gcd(a, b)
+        a, b = a.abs, b.abs
+        while b != 0
+            a, b = b, a % b
+        end
+        a
+    end
 end
